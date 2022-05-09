51f1b1be3d52a38a918701a598f70ae9afed0fb5
nico_qwer <100895510+nico-qwer@users.noreply.github.com>
2022-05-09 12:52:15 -0400

    script tag top ham menu

--- Scripts/HamburgerMenu.js
+++
@@ -1,8 +0,0 @@
-function toggleHamburger() {
-    var x = document.getElementById("links");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
